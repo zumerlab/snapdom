c5040d90b6276daa04e919ca4b0ecdf205f73af9
Juan Martin <martinmuda@gmail.com>
2025-05-14 15:44:26 +0300

    Delete functions

--- src/utils/minifySVG.js
+++
@@ -1,8 +0,0 @@
-/**
- * Removes unnecessary whitespace from SVG string
- * @param {string} svgString - SVG XML string
- * @returns {string} Minified SVG string
- */
-export function minifySVG(svgString) {
-  return svgString.replace(/>\s+</g, '><').replace(/\s{2,}/g, ' ').replace(/\n/g, '').trim();
-}
