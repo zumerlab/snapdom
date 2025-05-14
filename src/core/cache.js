091484c00941822684afc9148a59cb23e4b34627
Juan Martin <martinmuda@gmail.com>
2025-05-14 15:41:01 +0300

    Add cache Maps

---
+++ src/core/cache.js
@@ -0,0 +1,7 @@
+
+export const imageCache = new Map();
+export const bgCache = new Map();
+export const resourceCache = new Map();
+export const defaultStylesCache = new Map();
+export const baseCSSCache = new Map();
+export const computedStyleCache = new WeakMap();
