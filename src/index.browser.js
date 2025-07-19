134f24d714cfedb3b40034f43c97bcbc2cfaf517
Juan Martin <martinmuda@gmail.com>
2025-07-19 19:03:37 +0300

    Update browser entry file. See #119

--- src/index.browser.js
+++ src/index.browser.js
@@ -7,30 +7,7 @@
 import { snapdom } from './api/snapdom.js';
 import { preCache } from './api/preCache.js';
 
-window.snapdom = snapdom;
-
-// Global browsers
 if (typeof window !== 'undefined') {
   window.snapdom = snapdom;
   window.preCache = preCache;
 }
-
-// CommonJS (Angular, Node, etc)
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports = { snapdom, preCache };
-}
-
-// AMD 
-if (typeof define === 'function' && define.amd) {
-  define([], function () {
-    return { snapdom, preCache };
-  });
-}
-
-// Fallback
-if (typeof globalThis !== 'undefined') {
-  globalThis.snapdom = snapdom;
-  globalThis.preCache = preCache;
-}
-
-
