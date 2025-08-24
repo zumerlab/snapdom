91a554ecb696810591b3916cb14be21074773eb9
Juan Martin <martinmuda@gmail.com>
2025-08-24 22:53:24 +0300

    update exports to avoid vitest fails

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export * from './image';
-export * from './css';
-export * from './browser';
-export * from './helpers';
+export {createBackground, inlineSingleBackgroundEntry, fetchImage} from './image';
+export {precacheCommonTags, getDefaultStyleForTag, getStyleKey, collectUsedTagNames, generateDedupedBaseCSS, generateCSSClasses, getStyle, parseContent, snapshotComputedStyle, splitBackgroundImage} from './css';
+export {idle, isSafari} from './browser';
+export {fetchResource, safeEncodeURI, stripTranslate, isIconFont, extractURL} from './helpers';
