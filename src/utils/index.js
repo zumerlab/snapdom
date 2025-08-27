51ef80d24b693ceee3e33d75e2b64ba7037e49ea
Juan Martin <martinmuda@gmail.com>
2025-08-27 17:12:29 +0300

    update to avoid vitest issues

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export {createBackground, inlineSingleBackgroundEntry, fetchImage} from './image';
-export {precacheCommonTags, getDefaultStyleForTag, getStyleKey, collectUsedTagNames, generateDedupedBaseCSS, generateCSSClasses, getStyle, parseContent, snapshotComputedStyle, splitBackgroundImage} from './css';
-export {idle, isSafari} from './browser';
-export {fetchResource, safeEncodeURI, stripTranslate, isIconFont, extractURL} from './helpers';
+export {createBackground, inlineSingleBackgroundEntry, fetchImage} from './image.js';
+export {precacheCommonTags, getDefaultStyleForTag, getStyleKey, collectUsedTagNames, generateDedupedBaseCSS, generateCSSClasses, getStyle, parseContent, snapshotComputedStyle, splitBackgroundImage, NO_CAPTURE_TAGS, NO_DEFAULTS_TAGS} from './css.js';
+export {idle, isSafari} from './browser.js';
+export {fetchResource, safeEncodeURI, stripTranslate, isIconFont, extractURL} from './helpers.js';
